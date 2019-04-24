331554399c000392ed31a69915790e3bc514797f
CEM ONUR BAS <cemonurbas@CEM-MacBook-Pro.local>
2019-04-24 22:09:12 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let bookingData = {
+    unit: 3,
+    people: 5,
+    from: 20,
+    to: 22,
+    cash: 300
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const calculateTip = (cash) => {
+    if(cash<50)
+        return cash * 0.05
+    else
+        return cash * 0.1
+}
+
+
+const Booking = ({unit, people, from, to, cash}) => {
+	return (
+		<section>
+				<div>
+					<p>Table No: {unit}</p>
+				</div>
+				<div>
+					<p>People: {people}</p>
+				</div>
+				<div>
+					<p>From: {from}:00</p>
+				</div>
+				<div>
+					<p>To: {to}:00</p>
+				</div>
+				<div>
+					<p>Cash: {cash}</p>
+				</div>
+				<div>
+					<p>Tip: {calculateTip(cash)}</p>
+				</div>
+		</section>
+	)
+}
+
+render(
+	<Booking 
+		unit={bookingData.unit}
+		people={bookingData.people}
+		from={bookingData.from}
+		to={bookingData.to}
+		cash={bookingData.cash}/>, 
+	document.getElementById('root')
+)
