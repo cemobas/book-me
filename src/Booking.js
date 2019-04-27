a45a01bc37bc48eade6e6f9adebe7a7480d17805
CEM ONUR BAS <cemonurbas@CEM-MacBook-Pro.local>
2019-04-27 13:23:44 +0200

    Add more React functionality (1) to follow the tips on https://jscomplete.com/learn/react-beyond-basics/react-fundamentals

--- src/Booking.js
+++ src/Booking.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+const NameDisplay = ({name, gender}) => (
+	<div>
+		Greetings {gender === 'M' ? "Mr. " : "Ms. "} {name}
+	</div>
+)
+
+const Koniec = ({name, gender, text}) => (
+	/* Try to delete <div>, it won't work. */
+	<div>
+		{text}
+		{/* You can even use a React element inside JSX, because that too is an expression. Remember, a React element is just a function call. If no gender is blank, no greeting statement will be displayed. (Try erasing M in gender.) */}
+		{ gender && <NameDisplay name={name} gender={gender}/> }
+	</div>
+)
+
+/* Every component gets a special instance property called props that holds all values passed to that component’s element when it was instantiated. Unlike function components, the render function in class-based components does not receive any arguments. */
 class Booking extends React.Component {
 
 	state = {
+		name: "Cem",
+		gender: "M",
 		tip: false
 	}
 	
@@ -35,12 +53,15 @@ class Booking extends React.Component {
 				<div>
 					<p>To: {to}:00</p>
 				</div>
-				<div>
+				{ /* Sometimes we use a JavaScript object inside curly brackets to pass a CSS style object to the special style attribute in React. */}
+				<div style={ { color:'yellow', backgroundColor:'navy' } }>
 					<p>Cash: {cash}</p>
 				</div>
 				<div>
+					{/* This is equivalent to the ${} interpolation syntax in JavaScript template literals. This is the only constraint inside JSX: Only expressions. For example, you cannot use a regular if-statement, but a ternary expression is okay. JavaScript variables are also expressions. */}
 					<button onClick={this.toggleTip}>{this.state.tip ? "Cancel tip" : "Add tip" }</button>
 					<p>Tip: {this.state.tip ? this.calculateTip(cash) : "no tip" }</p>
+					<Koniec name={this.state.name} gender={this.state.gender} text="Your booking has been confirmed."/>
 				</div>
 			</section>
 		)
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom'
 import Booking from './Booking'
 
+/* ReactDOM renders DOM elements backed by instances of their components. This is true for class components. For function components, ReactDOM renders just DOM elements. Function components do not have instances (that can be accessed with this), so when using a function component, ReactDOM renders a DOM element generated from the function’s returned element. */
 render(
 	<Booking people="5" from="20" to="22" cash="300" />, 
 	document.getElementById('root')
